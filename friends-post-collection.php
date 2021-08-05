46ce7f086245391fb0fcd8f2295dc1eb0eceea67
Alex Kirk <akirk@users.noreply.github.com>
2021-08-05 16:29:49 +0200

    Version bump

--- friends-post-collection.php
+++ friends-post-collection.php
@@ -3,7 +3,7 @@
  * Plugin name: Friends Post Collection
  * Plugin author: Alex Kirk
  * Plugin URI: https://github.com/akirk/friends-post-collection
- * Version: 0.4
+ * Version: 0.5
  *
  * Description: Collect posts from around the web into your Friends UI.
  *
